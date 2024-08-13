dfe4086cea7fd6ce1bd4185f62a7e2708a195ecd
Amal Masood <amalmasood2004@gmail.com>
2024-08-13 23:40:31 +0300

    changed a lot of things

---
+++ scripts.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+let autoSlideTimer;
+
+showSlides(slideIndex);
+autoSlides();
+
+function plusSlides(n) {
+    clearTimeout(autoSlideTimer); // Stop the auto slide timer
+    showSlides(slideIndex += n);
+    autoSlides(); // Restart the auto slide timer after manual navigation
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
+
+function autoSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    autoSlideTimer = setTimeout(autoSlides, 3000); // Change slide every 3 seconds
+}
